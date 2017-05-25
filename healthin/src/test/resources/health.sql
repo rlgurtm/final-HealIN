4846e359127fc791676c1ca3f0fcfb3327b5ccef
Administrator <Administrator@192.168.0.175>
2017-05-25 17:11:48 +0900

    지원이 sql 수정 - 5시 11 분

--- healthin/src/test/resources/health.sql
+++ healthin/src/test/resources/health.sql
@@ -2,21 +2,26 @@
 
 
 -- 아래쪽부터 드랍하세요!
-drop table health_member;
-drop table health_user;
-drop table trainer;
-drop table tipandqna;
-drop table health_comment;
-drop table matching;
-drop table mentoring;
 drop table exercise;
 drop table consumption_member;
 drop table physical_info;
 drop table food;
 drop table intake_member;
+drop table matching;
 drop table following;
+drop table mentoring;
 drop table pay;
 drop table trainer_rate;
+drop table field_category;
+drop table field;
+drop table video_comment;
+drop table video_like;
+drop table trainer_video;
+drop table health_user;
+drop table trainer;
+drop table health_comment;
+drop table tipandqna;
+drop table health_member;
 
 drop sequence board_no_seq;
 drop sequence comment_no_seq;
@@ -27,6 +32,7 @@ drop sequence intake_no_seq;
 drop sequence pay_no_seq;
 drop sequence video_no_seq;
 
+
 -- 회원 테이블
 drop table health_member;
 create table health_member(
@@ -34,7 +40,7 @@ create table health_member(
 	password varchar2(100) not null,
 	nickname varchar2(100) not null,
 	name varchar2(100) not null,
-	birthdate number default 0, 
+	birthdate varchar2(100) not null, 
 	gender varchar2(100) not null,
 	address varchar2(100) not null,
 	tel varchar2(100) not null,
@@ -139,7 +145,7 @@ create table consumption_member(
 	constraint fk_health_name foreign key(name) references exercise(name)
 );
 -- 회원별 소비량 시퀀스
-drop sequence consumpton_no_seq;
+drop sequence consumption_no_seq;
 create sequence consumption_no_seq;
 
 --회원 체중관리 테이블
