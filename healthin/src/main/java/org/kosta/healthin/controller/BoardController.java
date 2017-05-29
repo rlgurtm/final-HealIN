125bda2ad3c32db93405369101318a9a3274a366
Administrator <Administrator@192.168.0.175>
2017-05-29 16:22:36 +0900

    지원 commit

--- healthin/src/main/java/org/kosta/healthin/controller/BoardController.java
+++ healthin/src/main/java/org/kosta/healthin/controller/BoardController.java
@@ -1,9 +1,13 @@
 package org.kosta.healthin.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.kosta.healthin.model.service.TipService;
+import org.kosta.healthin.model.service.TrainerService;
 import org.kosta.healthin.model.vo.ListVO;
+import org.kosta.healthin.model.vo.VO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +17,8 @@ public class BoardController {
 	
 	@Resource
 	private TipService tipService;
+	@Resource
+	private TrainerService trainerService;	
 	
 	@RequestMapping("tip/tip.do")
 	public String getTipBoardList(Model model,String nowpage){
@@ -23,4 +29,16 @@ public class BoardController {
 		return "tip/tip.tiles";
 	}
 
+	
+	@RequestMapping("trainer/trainerList.do")
+	public String gettrainerList(Model model,String pageNo){
+		if(pageNo==null)
+			pageNo="1";
+		List<VO> list=trainerService.getTrainerList(pageNo);
+		System.out.println("trainer Controller : "+list);
+		model.addAttribute("list",list);
+		return "trainer/trainerList.tiles";
+		
+		
+	}
 }
--- healthin/src/main/java/org/kosta/healthin/model/dao/TrainerDAOImpl.java
+++ healthin/src/main/java/org/kosta/healthin/model/dao/TrainerDAOImpl.java
@@ -1,8 +1,27 @@
 package org.kosta.healthin.model.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.kosta.healthin.model.vo.VO;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class TrainerDAOImpl implements TrainerDAO {
+	@Resource
+	private SqlSessionTemplate template;
+	
+	@Override
+	public int getTrainerTotalCount() {
+		return template.selectOne("trainer.getTrainerTotalCount");
+	}
+
+	@Override
+	public List<VO> getTrainerList(Map<String, Integer> map) {
+		return template.selectList("trainer.getTrainerList",map);
+	}
 
 }
