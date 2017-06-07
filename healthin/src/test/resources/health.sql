b0a7f484f5dfc474859d635542548c2758702c1c
KOSTA <KOSTA@192.168.0.151>
2017-06-07 17:37:11 +0900

    commit 송희

--- healthin/src/test/resources/health.sql
+++ healthin/src/test/resources/health.sql
@@ -92,7 +92,7 @@ create table health_comment(
 	health_comment clob not null,
 	posted_date date not null,
 	id varchar2(100) not null
-	constraint member_fk_comment references health_member(id),
+	constraint member_fk_comment references health_member(id)
 )
 alter table health_comment add constraint qna_fk_board foreign key (board_no) 
 references tipandqna(board_no) on delete cascade
