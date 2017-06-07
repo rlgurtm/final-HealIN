0165ce7365c5ebf5f90192038e26b3b1906630de
KOSTA <KOSTA@192.168.0.113>
2017-06-07 17:46:25 +0900

    myPage PagingBean -- commit

--- healthin/src/main/java/org/kosta/healthin/model/service/MyPageService.java
+++ healthin/src/main/java/org/kosta/healthin/model/service/MyPageService.java
@@ -2,6 +2,7 @@ package org.kosta.healthin.model.service;
 
 import org.kosta.healthin.model.vo.ListVO;
 import org.kosta.healthin.model.vo.MemberVO;
+import org.kosta.healthin.model.vo.PagingBean;
 import org.kosta.healthin.model.vo.PhysicalInfoVO;
 
 public interface MyPageService {
@@ -9,5 +10,9 @@ public interface MyPageService {
 	void insertUserPhysicalInfo(PhysicalInfoVO pivo);
 
 	ListVO selectUserPhysicalInfo(MemberVO mvo);
+
+	PhysicalInfoVO graphPagingBean(PagingBean pb);
+
+	int graphContentCount();
 
 }
