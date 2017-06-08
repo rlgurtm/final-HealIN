db93f6713b2db7d87e2dfc6c561f8bc7a9e87fa3
KOSTA <KOSTA@192.168.0.113>
2017-06-08 17:38:53 +0900

    그래프 변경,jsp a태그 commit

--- healthin/src/main/java/org/kosta/healthin/controller/BmiGraphController.java
+++ healthin/src/main/java/org/kosta/healthin/controller/BmiGraphController.java
@@ -3,7 +3,6 @@ package org.kosta.healthin.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class BMIGraphController {
-	/*"${pageContext.request.contextPath }/graphPagingBean.do"*/
+public class BmiGraphController {
 	
 }
