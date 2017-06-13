1af95cd9b41297b61d30433f1d74fa3c15705086
Administrator <Administrator@192.168.0.142>
2017-06-13 16:03:14 +0900

    mentoring commit JKH

--- healthin/src/main/java/org/kosta/healthin/model/service/MentoringService.java
+++ healthin/src/main/java/org/kosta/healthin/model/service/MentoringService.java
@@ -1,11 +1,18 @@
 package org.kosta.healthin.model.service;
 
 import org.kosta.healthin.model.vo.ListVO;
+import org.kosta.healthin.model.vo.MentoringVO;
 
 public interface MentoringService {
 
 	ListVO findByTrainerMatchingMemberList(String trainerId);
 
 	ListVO findByUserMatchingMemberList(String userId);
+
+	ListVO mentoringDetail(MentoringVO mentoring);
+
+	void insertMentoring(MentoringVO mentoring);
+
+	void mentoringDetailHits(MentoringVO mentoring);
 
 }
